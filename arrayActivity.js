ab3e65eb2572ef30adb9df68dfc19b24a5843e9a
Villanueva, Robert G. <bertvillxxxxva@gmail.com>
2024-10-16 05:44:23 -0800

    Add files via upload

---
+++ arrayActivity.js
@@ -0,0 +1,12 @@
+const favSingers = ["Taylor Swift", "Nateman", "Hevabi"];
+
+console.log(favSingers[1]); 
+
+const favNumbers = [7, 13, 21, 37];
+
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(mixedArr[0]);
+console.log(mixedArr[1]); 
+console.log(mixedArr[2]); 
+console.log(mixedArr[3]); 
